f1ecdfa4a8b21f5f28c4e2f43d1ead2810ef3cec
lovkesh <lovkesh.handa@softuvo.com>
2018-04-30 17:30:24 +0530

    changes

--- app/src/main/java/com/grandgroup/activities/RiskReportActivity.java
+++ app/src/main/java/com/grandgroup/activities/RiskReportActivity.java
@@ -1,15 +1,162 @@
 package com.grandgroup.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.grandgroup.R;
+import com.grandgroup.utills.CommonUtils;
+import com.grandgroup.utills.PermissionUtils;
+import com.grandgroup.views.CustomDateDialog;
+import com.grandgroup.views.CustomTimeDialog;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RiskReportActivity extends AppCompatActivity {
+
+    public final int CAMERA_PERMISSIONS_REQUEST = 123;
+    public final int GALLERY_PERMISSIONS_REQUEST =124;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_event_date)
+    TextView tv_event_date;
+    @BindView(R.id.iv_image)
+    ImageView iv_image;
+    private AppCompatActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risk_report);
+        setInitialData();
+    }
+
+    @OnClick({R.id.tv_event_date, R.id.lay_photo})
+    public void onclick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_event_date:
+                CustomDateDialog.getInstance().DatePicker(mContext, new CustomDateDialog.DateDialogListener() {
+                    @Override
+                    public void onOkayClick(int date, int month, int year) {
+                        CustomTimeDialog.getInstance().timePicker(mContext, new CustomTimeDialog.TimeDialogListener() {
+
+                            @Override
+                            public void onOkayClick(String twentyFourTime, String TwelveHourTime) {
+
+                            }
+                        });
+                    }
+                });
+                break;
+            case R.id.lay_photo:
+                selectImage();
+                break;
+        }
+
+    }
+
+    private void setInitialData() {
+        mContext = RiskReportActivity.this;
+        ButterKnife.bind(mContext);
+        tvTitle.setText("Risk / Hazard Report");
+    }
+
+
+    private void selectImage() {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (options[item].equals("Take Photo")) {
+                            if (PermissionUtils.requestPermission(mContext, CAMERA_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)) {
+                                startCamera();
+                            }
+                        } else if (options[item].equals("Choose from Gallery")) {
+                            if (PermissionUtils.requestPermission(mContext, GALLERY_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                                GalleryIntent();
+                            }
+
+                        } else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+
+
+                if (data != null) {
+                    Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+
+                    Uri tempUri = CommonUtils.getInstance().getImageUri(mContext, thumbnail);
+                    iv_image.setImageBitmap(thumbnail);
+                }
+
+            } else if (requestCode == 2) {
+                if (data != null) {
+                    Bitmap bitmap = null;
+                    try {
+                        Uri tempUri = data.getData();
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), tempUri);
+                        iv_image.setImageBitmap(bitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+                    startCamera();
+                }
+            case GALLERY_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+                    GalleryIntent();
+                }
+                break;
+        }
+    }
+
+    private void startCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        startActivityForResult(cameraIntent, 1);
+    }
+
+    private void GalleryIntent(){
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
     }
 }
--- app/src/main/java/com/grandgroup/activities/UserProfileActivity.java
+++ app/src/main/java/com/grandgroup/activities/UserProfileActivity.java
@@ -10,11 +10,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -42,26 +42,20 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class UserProfileActivity extends BaseActivity {
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.iv_user_pic)
+    ImageView ivUserPic;
+    @BindView(R.id.et_username)
+    EditText etUserName;
+    @BindView(R.id.et_pswd)
+    EditText etPassword;
+    @BindView(R.id.et_email)
+    EditText etEmail;
     private AppCompatActivity mContext;
     private ParseUser parseUser;
     private UserProfileBean userProfileObj;
     private Bitmap scaled = null;
-
-
-    @BindView(R.id.tv_title)
-    TextView tvTitle;
-
-    @BindView(R.id.iv_user_pic)
-    ImageView ivUserPic;
-
-    @BindView(R.id.et_username)
-    EditText etUserName;
-
-    @BindView(R.id.et_pswd)
-    EditText etPassword;
-
-    @BindView(R.id.et_email)
-    EditText etEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,167 +95,169 @@ public class UserProfileActivity extends BaseActivity {
                 break;
             case R.id.btn_save:
                 finish();
-            //    updateProfile();
+                //    updateProfile();
                 break;
             case R.id.tv_tv_change_pic:
                 selectImage();
         }
     }
-        private void selectImage () {
-            final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
-            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-            builder.setTitle("Add Photo!");
-            builder.setItems(options,
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int item) {
-                            if (options[item].equals("Take Photo")) {
-                                if (checkPermissionCamera()) {
-                                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                                    cameraIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-                                    startActivityForResult(cameraIntent, 1);
-                                }
-                            } else if (options[item].equals("Choose from Gallery")) {
-                                if (checkPermission()) {
-                                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                                    startActivityForResult(intent, 2);
-                                }
 
-                            } else if (options[item].equals("Cancel")) {
-                                dialog.dismiss();
+    private void selectImage() {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (options[item].equals("Take Photo")) {
+                            if (checkPermissionCamera()) {
+                                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                                cameraIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                                startActivityForResult(cameraIntent, 1);
+                            }
+                        } else if (options[item].equals("Choose from Gallery")) {
+                            if (checkPermission()) {
+                                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(intent, 2);
+                            }
+
+                        } else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+        builder.show();
+    }
+
+    private boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("Test", "Permission is granted");
+                return true;
+            } else {
+                Log.v("Test", "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                return true;
+            }
+        } else {
+            Log.v("Test", "Permission is granted");
+            return true;
+        }
+    }
+
+    private boolean checkPermissionCamera() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("Test", "Permission is granted");
+                return true;
+            } else {
+                Log.v("Test", "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            Log.v("Test", "Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                int nh = (int) (photo.getHeight() * (512.0 / photo.getWidth()));
+                scaled = Bitmap.createScaledBitmap(photo, 512, nh, true);
+                ivUserPic.setImageBitmap(scaled);
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                int nh = (int) (thumbnail.getHeight() * (512.0 / thumbnail.getWidth()));
+                scaled = Bitmap.createScaledBitmap(thumbnail, 512, nh, true);
+                ivUserPic.setImageBitmap(scaled);
+            }
+        }
+    }
+
+
+    private void updateProfile(final String userFirstName, final String userLastName,
+                               final String userEmail, final Bitmap scaled) {
+        parseUser = ParseUser.getCurrentUser();
+
+        parseUser.getParseObject("User").fetchIfNeededInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, com.parse.ParseException e) {
+                if (e == null) {
+                    parseUser.saveInBackground();
+                    object.put(getString(R.string.userFirstName), userFirstName);
+                    object.put(getString(R.string.userLastName), userLastName);
+                    object.put(getString(R.string.userEmail), userEmail);
+
+                    if (scaled != null) {
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        scaled.compress(Bitmap.CompressFormat.PNG, 70, stream);
+                        byte[] image = stream.toByteArray();
+                        ParseFile file = new ParseFile("image.png", image);
+                        object.put(getString(R.string.profilePic), file);
+                    }
+                    object.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+
+                                parseUser.getParseObject("User").fetchIfNeededInBackground(new GetCallback<ParseObject>() {
+                                    @Override
+                                    public void done(ParseObject object, com.parse.ParseException e) {
+                                        if (e == null) {
+                                            parseUser.saveInBackground();
+                                            object.put(getString(R.string.userFirstName), userFirstName);
+                                            object.put(getString(R.string.userLastName), userLastName);
+                                            object.put(getString(R.string.userEmail), userEmail);
+
+                                            if (scaled != null) {
+                                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                                                scaled.compress(Bitmap.CompressFormat.PNG, 70, stream);
+                                                byte[] image = stream.toByteArray();
+                                                ParseFile file = new ParseFile("image.png", image);
+                                                object.put(getString(R.string.profilePic), file);
+                                            }
+                                            ParseFile postImage = object.getParseFile("profilePic");
+                                            if (postImage != null)
+                                                userProfileObj.setUserProfilePicUrl(postImage.getUrl());
+                                            else
+                                                userProfileObj.setUserProfilePicUrl("");
+
+                                            Gson gson = new Gson();
+                                            String json = gson.toJson(userProfileObj);
+                                            AppPrefrence.init(mContext).putString(AppConstant.USER_PROFILE, json);
+                                        }
+                                    }
+                                });
+
+                                //  BloodDonationHelper.bloodDonationHelper(mContext).dismissLoader();
+                                Toast.makeText(getApplicationContext(), "Successfully Updated", Toast.LENGTH_LONG).show();
+                            } else {
+                                //  BloodDonationHelper.bloodDonationHelper(mContext).dismissLoader();
+                                Toast.makeText(getApplicationContext(), "Please, Try Again", Toast.LENGTH_LONG).show();
+
+
                             }
                         }
                     });
-            builder.show();
-        }
-
-        private boolean checkPermission () {
-            if (Build.VERSION.SDK_INT >= 23) {
-                if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    Log.v("Test", "Permission is granted");
-                    return true;
                 } else {
-                    Log.v("Test", "Permission is revoked");
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-                    return true;
-                }
-            } else {
-                Log.v("Test", "Permission is granted");
-                return true;
-            }
-        }
-        private boolean checkPermissionCamera () {
-            if (Build.VERSION.SDK_INT >= 23) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    Log.v("Test", "Permission is granted");
-                    return true;
-                } else {
-                    Log.v("Test", "Permission is revoked");
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                    return false;
-                }
-            } else {
-                Log.v("Test", "Permission is granted");
-                return true;
-            }
-        }
-
-        @Override
-        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
-            super.onActivityResult(requestCode, resultCode, data);
-            if (resultCode == RESULT_OK) {
-                if (requestCode == 1) {
-                    Bitmap photo = (Bitmap) data.getExtras().get("data");
-                    int nh = (int) (photo.getHeight() * (512.0 / photo.getWidth()));
-                    scaled = Bitmap.createScaledBitmap(photo, 512, nh, true);
-                    ivUserPic.setImageBitmap(scaled);
-                } else if (requestCode == 2) {
-                    Uri selectedImage = data.getData();
-                    String[] filePath = {MediaStore.Images.Media.DATA};
-                    Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
-                    c.moveToFirst();
-                    int columnIndex = c.getColumnIndex(filePath[0]);
-                    String picturePath = c.getString(columnIndex);
-                    c.close();
-                    Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-                    int nh = (int) (thumbnail.getHeight() * (512.0 / thumbnail.getWidth()));
-                    scaled = Bitmap.createScaledBitmap(thumbnail, 512, nh, true);
-                    ivUserPic.setImageBitmap(scaled);
+                    //   BloodDonationHelper.bloodDonationHelper(mContext).dismissLoader();
+                    Toast.makeText(getApplicationContext(), "Please, Try Again", Toast.LENGTH_LONG).show();
                 }
             }
-        }
-
-
-        private void updateProfile ( final String userFirstName, final String userLastName,
-                                     final String userEmail, final Bitmap scaled){
-            parseUser = ParseUser.getCurrentUser();
-
-            parseUser.getParseObject("User").fetchIfNeededInBackground(new GetCallback<ParseObject>() {
-                @Override
-                public void done(ParseObject object, com.parse.ParseException e) {
-                    if (e == null) {
-                        parseUser.saveInBackground();
-                        object.put(getString(R.string.userFirstName), userFirstName);
-                        object.put(getString(R.string.userLastName), userLastName);
-                        object.put(getString(R.string.userEmail), userEmail);
-
-                        if (scaled != null) {
-                            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                            scaled.compress(Bitmap.CompressFormat.PNG, 70, stream);
-                            byte[] image = stream.toByteArray();
-                            ParseFile file = new ParseFile("image.png", image);
-                            object.put(getString(R.string.profilePic), file);
-                        }
-                        object.saveInBackground(new SaveCallback() {
-                            @Override
-                            public void done(ParseException e) {
-                                if (e == null) {
-
-                                    parseUser.getParseObject("User").fetchIfNeededInBackground(new GetCallback<ParseObject>() {
-                                        @Override
-                                        public void done(ParseObject object, com.parse.ParseException e) {
-                                            if (e == null) {
-                                                parseUser.saveInBackground();
-                                                object.put(getString(R.string.userFirstName), userFirstName);
-                                                object.put(getString(R.string.userLastName), userLastName);
-                                                object.put(getString(R.string.userEmail), userEmail);
-
-                                                if (scaled != null) {
-                                                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                                                    scaled.compress(Bitmap.CompressFormat.PNG, 70, stream);
-                                                    byte[] image = stream.toByteArray();
-                                                    ParseFile file = new ParseFile("image.png", image);
-                                                    object.put(getString(R.string.profilePic), file);
-                                                }
-                                                ParseFile postImage = object.getParseFile("profilePic");
-                                                if (postImage != null)
-                                                    userProfileObj.setUserProfilePicUrl(postImage.getUrl());
-                                                else
-                                                    userProfileObj.setUserProfilePicUrl("");
-
-                                                Gson gson = new Gson();
-                                                String json = gson.toJson(userProfileObj);
-                                                AppPrefrence.init(mContext).putString(AppConstant.USER_PROFILE, json);
-                                            }
-                                        }
-                                    });
-
-                                    //  BloodDonationHelper.bloodDonationHelper(mContext).dismissLoader();
-                                    Toast.makeText(getApplicationContext(), "Successfully Updated", Toast.LENGTH_LONG).show();
-                                } else {
-                                    //  BloodDonationHelper.bloodDonationHelper(mContext).dismissLoader();
-                                    Toast.makeText(getApplicationContext(), "Please, Try Again", Toast.LENGTH_LONG).show();
-
-
-                                }
-                            }
-                        });
-                    } else {
-                        //   BloodDonationHelper.bloodDonationHelper(mContext).dismissLoader();
-                        Toast.makeText(getApplicationContext(), "Please, Try Again", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        }
-
+        });
     }
+
+}
